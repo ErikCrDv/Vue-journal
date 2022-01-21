29900327cf3290e83c560a53fa4d7b729e0564e5
Erik Cordova <ErikCrDv@outlook.com>
2022-01-20 23:15:12 -0600

    Delete Key

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const authApi = axios.create({
     baseURL: 'https://identitytoolkit.googleapis.com/v1/accounts',
     params: {
-        key: 'AIzaSyA1V8VnUhGvKIZmCcI3tM5bdu-HX6EH9oU'
+        key: ''
     }
 })
 
